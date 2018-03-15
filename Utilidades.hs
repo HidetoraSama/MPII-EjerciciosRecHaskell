3431c948ad9a9d8476d00b3ccce41f4050417774
HidetoraSama <zatchflyff@gmail.com>
2018-03-15 05:51:13 -0500

    enuplar

--- Utilidades.hs
+++ Utilidades.hs
@@ -20,6 +20,9 @@ sumaLista (b:bs) = b + sumaLista (bs)
 doblarLista::[Int]->[Int]
 doblarLista (x:xs) = [2*x | x <- (x:xs)]
 
+enuplarLista::Int->[Int]->[Int]
+enuplarLista n (x:xs) = [n*x | x <- (x:xs)]
+
 cuentaApariciones::[Int]->Int->Int
 cuentaApariciones [] x = 0
 cuentaApariciones (b:bs) x = if b == x then 1 + cuentaApariciones (bs) x
